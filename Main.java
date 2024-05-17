60801fd776648e4ddb2088030e2e2896027e68a3
Izan Lozano <151909866+izaann3@users.noreply.github.com>
2024-05-17 08:57:53 +0200

    modificamos el main para que haga data de vencimineto i descripcion

--- Main.java
+++ Main.java
@@ -1,21 +1,22 @@
 package taskmanagement;
 
-class Main {
+public class Main {
 	public static void main(String[] args) {
 		TaskManagement manager = new TaskManagement();
+        
 		System.out.println("Add task");
-		manager.addTask("Tarea de GBD", "Alta");
-		System.out.println("Hacemos Print de todo");
-		manager.printAllTask();
-		System.out.println("Hacemos Print de prioridad");
-		manager.printTaskByPriority("Alta");
-		System.out.println("Hacemos que la tarea este done");
-		manager.doneTask(1);
-		System.out.println("Hacemos Print de todo");
-		manager.printAllTask();
-		System.out.println("Eliminamos la tarea");
-		manager.deleteTask(1);
-		System.out.println("Hacemos Print de todo");
-		manager.printAllTask();
-	}
+        	manager.addTask("Tarea GBD", "Alta", "Hacer los ejercicios de BBDD", "2024-05-31");
+        	System.out.println("Hacemos Print de todo");
+        	manager.printAllTask();
+        	System.out.println("Hacemos Print de prioridad");
+       	 	manager.printTaskByPriority("Alta");
+        	System.out.println("Hacemos que la tarea esté done");
+        	manager.doneTask(1);
+        	System.out.println("Hacemos Print de todo");
+       		manager.printAllTask();
+        	System.out.println("Eliminamos la tarea");
+        	manager.deleteTask(1);
+        	System.out.println("Hacemos Print de todo");
+        	manager.printAllTask();
+    }
 }
