78eba02ecf82adc73475371fd370c8656e66c7a5
Izan Lozano <151909866+izaann3@users.noreply.github.com>
2024-05-17 08:58:37 +0200

    ahora cambiamos las classes taskmanager y task con la data de vencimiento y la descripcion

--- TaskManagement.java
+++ TaskManagement.java
@@ -4,75 +4,93 @@ import java.util.HashMap;
 import java.util.ArrayList;
 
 class TaskManagement {
-	HashMap<String, ArrayList<Task>> lists;
-	HashMap<Integer, Task> tasks;
+    HashMap<String, ArrayList<Task>> lists;
+    HashMap<Integer, Task> tasks;
 
-	public TaskManagement() {
-		lists = new HashMap<String, ArrayList<Task>>();
-		lists.put("Baja", new ArrayList<Task>());
-		lists.put("Media", new ArrayList<Task>());
-		lists.put("Alta", new ArrayList<Task>());
-		tasks = new HashMap<Integer, Task>();
-	}
+    public TaskManagement() {
+        lists = new HashMap<String, ArrayList<Task>>();
+        lists.put("Baja", new ArrayList<Task>());
+        lists.put("Media", new ArrayList<Task>());
+        lists.put("Alta", new ArrayList<Task>());
+        tasks = new HashMap<Integer, Task>();
+    }
 
-	public void addTask(String taskName, String priority) {
-		Task task = new Task(taskName);
-		lists.get(priority).add(task);
-		tasks.put(task.getId(), task);
-	}
+    public void addTask(String taskName, String priority, String description, String dueDate) {
+        Task task = new Task(taskName, priority, description, dueDate);
+        lists.get(priority).add(task);
+        tasks.put(task.getId(), task);
+    }
 
-	public void doneTask(int taskId) {
-		tasks.get(taskId).done();
-	}
+    public void doneTask(int taskId) {
+        tasks.get(taskId).done();
+    }
 
-	public void deleteTask(int taskId) {
-		tasks.remove(taskId);
-	}
+    public void deleteTask(int taskId) {
+        tasks.remove(taskId);
+    }
 
-	public void printAllTask() {
-		tasks.forEach((key, task) -> {
-			System.out.println(task);
-		}
-		);
-	}
+    public void printAllTask() {
+        tasks.forEach((key, task) -> {
+            System.out.println(task);
+        });
+    }
 
-	public void printTaskByPriority(String priority) {
-		System.out.println("Tareas de prioridad: " + priority);
-		for (Task task: lists.get(priority)) {
-			System.out.println(task);
-		}
-	}
+    public void printTaskByPriority(String priority) {
+        System.out.println("Tareas de prioridad: " + priority);
+        for (Task task : lists.get(priority)) {
+            System.out.println(task);
+        }
+    }
 }
 
 class Task {
-	private static int numInstances = 0;
-	private int id;
-	private String name;
-	private boolean isDone;
+    private static int numInstances = 0;
+    private int id;
+    private String title;
+    private String priority;
+    private String description;
+    private String dueDate;
+    private boolean isDone;
 
-	public Task(String name) {
-		this.name = name;
-		this.isDone = false;
-		this.id = ++numInstances;
-	}
+    public Task(String title, String priority, String description, String dueDate) {
+        this.title = title;
+        this.priority = priority;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.isDone = false;
+        this.id = ++numInstances;
+    }
 
-	public void done() {
-		this.isDone = true;
-	}
+    public void done() {
+        this.isDone = true;
+    }
 
-	public int getId() {
-		return this.id;
-	}
+    public int getId() {
+        return this.id;
+    }
 
-	public String getName() {
-		return this.name;
-	}
+    public String getTitle() {
+        return this.title;
+    }
 
-	public boolean isDone() {
-		return this.isDone;
-	}
+    public String getPriority() {
+        return this.priority;
+    }
 
-	public String toString() {
-		return "Id:" + String.valueOf(id) + " Name: " + name + " Is done:" + String.valueOf(isDone);
-	}
+    public String getDescription() {
+        return this.description;
+    }
+
+    public String getDueDate() {
+        return this.dueDate;
+    }
+
+    public boolean isDone() {
+        return this.isDone;
+    }
+
+    public String toString() {
+        return "Id: " + id + "\nTitle: " + title + "\nPriority: " + priority + "\nDescription: " + description
+                + "\nDue Date: " + dueDate + "\nIs Done: " + isDone;
+    }
 }
